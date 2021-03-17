ba14ce3dccb1d6ae4e4cf4691a3e60b45fc0868b
Lo-GR <diamondintheroth@gmail.com>
2021-03-17 15:47:45 -0700

    make MovieTheaterContext
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

---
+++ MovieTheater/Models/MovieTheaterContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieTheater.Models
+{
+  public class MovieTheaterContext : DbContext
+  {
+    public DbSet<Customer> Customers {get; set;}
+    public DbSet<Movie> Movies {get; set;}
+    public DbSet<CustomerMovie> CustomerMovie {get; set;}
+    public MovieTheaterContext(DbContextOptions options) : base(options){}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadProxies();
+    }
+  }
+}
