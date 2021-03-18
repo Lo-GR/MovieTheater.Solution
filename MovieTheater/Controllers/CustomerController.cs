8fc8d944d4fce627b191db5053848a02a59f63c2
Lo-GR <diamondintheroth@gmail.com>
2021-03-18 13:52:57 -0700

    create constructor for Customer Controller
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- MovieTheater/Controllers/CustomerController.cs
+++ MovieTheater/Controllers/CustomerController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+using MovieTheater.Models;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MovieTheater.Controllers
 {
-  
+  public class CustomersController : Controller
+  {
+    private readonly MovieTheaterContext _db;
+    public CustomersController(MovieTheaterContext db)
+    {
+      _db = db;
+    }
+  }
 }
