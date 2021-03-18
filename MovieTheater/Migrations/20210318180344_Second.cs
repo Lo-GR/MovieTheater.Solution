8015b2f4ddf4b876d29a2bdce6fecbfe4bb49172
Lo-GR <diamondintheroth@gmail.com>
2021-03-18 11:06:13 -0700

    apply second migration
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

---
+++ MovieTheater/Migrations/20210318180344_Second.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieTheater.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeSlot",
+                table: "Movies",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeSlot",
+                table: "Movies",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+    }
+}
--- MovieTheater/Models/Movie.cs
+++ MovieTheater/Models/Movie.cs
@@ -8,7 +8,7 @@ namespace MovieTheater.Models
     public int MovieId {get; set;}
     public string Name {get; set;}
     public int MinAge {get; set;}
-    public DateTime TimeSlot {get; set;}
+    public string TimeSlot {get; set;}
     public virtual ICollection<CustomerMovie> JoinEntities {get;}
     
     public Movie()
